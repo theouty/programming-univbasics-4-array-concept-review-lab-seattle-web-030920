55b653225243825a23fedd4b6e2d42362c02066a
Aman Michael <aman.david.michael@gmail.com>
2020-03-02 11:32:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort
+  array[-1]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.sort 
+  array[1]
 end
